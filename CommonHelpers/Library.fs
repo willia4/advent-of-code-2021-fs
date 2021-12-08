0c8ae32a19b3ea781fd505e663f10545d5f5058f
James Williams <james@jameswilliams.me>
2021-12-07 22:08:53 -0500

    Start day 1

--- CommonHelpers/Library.fs
+++ CommonHelpers/Library.fs
@@ -1,6 +1,7 @@
 ﻿namespace CommonHelpers
 
 module Helpers =
+    
     let safeParseInt (s:string) =
         match System.Int32.TryParse s with
         | true, int -> int
--- day1/Program.fs
+++ day1/Program.fs
@@ -3,14 +3,48 @@ let readInput path =
     path
     |> System.IO.File.ReadAllLines
     |> Array.map CommonHelpers.Helpers.safeParseInt
-
+    |> Array.toSeq
+    
+let countIncreases (chunks: seq<seq<int>>): int =
+    let chunks = chunks |> Seq.map (Seq.sum)
+    printfn $"%A{chunks}"
+    if (chunks |> Seq.length) >= 2 then
+        let rec inner prev rest acc =
+            if (rest |> Seq.isEmpty |> not) then
+               let current = rest |> Seq.head
+               inner current (rest |> Seq.skip 1) (if current > prev then acc + 1 else acc)
+            else
+                acc
+        
+        inner (chunks |> Seq.head) (chunks |> Seq.skip 1) 0
+    else
+        0
+    
 let part1 (path:string) : unit =
     let inputs = path
                  |> readInput
-                 |> Seq.fold (fun values nextValue -> values) (0, 0)
+                 |> Seq.chunkBySize 1
+                 |> Seq.map (Array.toSeq)
+                 |> countIncreases
     
-    //|> Seq.iter (fun x -> printfn $"%d{x}")
+    printfn "%s" "Part 1"
+    printfn $"%s{path}"
+    printfn $"%d{inputs}"
+   
+let part2 (path:string) : unit =
+    let inputs = path
+                 |> readInput
+                 |> Seq.chunkBySize 3
+                 |> Seq.map (Array.toSeq)
+                 |> countIncreases
+    
+    printfn "%s" "Part 2"
+    printfn $"%s{path}"
+    printfn $"%d{inputs}" 
     
     
-    
-part1 "test_input.txt"
+//part1 "test_input.txt"
+//part1 "input.txt"
+
+part2 "test_input.txt"
+//part2  "input.txt"
