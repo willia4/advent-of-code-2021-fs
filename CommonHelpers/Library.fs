ed172c404dfa7523d0f72004bd46b27edc11dddc
James Williams <james@jameswilliams.me>
2021-12-07 20:15:27 -0500

    Start working on F#

---
+++ CommonHelpers/Library.fs
@@ -0,0 +1,8 @@
+﻿namespace CommonHelpers
+
+module Helpers =
+    let safeParseInt (s:string) =
+        match System.Int32.TryParse s with
+        | true, int -> int
+        | _ -> 0
+
---
+++ day1/Program.fs
@@ -0,0 +1,16 @@
+﻿
+let readInput path =
+    path
+    |> System.IO.File.ReadAllLines
+    |> Array.map CommonHelpers.Helpers.safeParseInt
+
+let part1 (path:string) : unit =
+    let inputs = path
+                 |> readInput
+                 |> Seq.fold (fun values nextValue -> values) (0, 0)
+    
+    //|> Seq.iter (fun x -> printfn $"%d{x}")
+    
+    
+    
+part1 "test_input.txt"
